acbc5ea92d6da1ce19f0c453f47ef00aae2f7e38
路赛 <lusai@outlook.com>
2021-03-23 14:24:15 +0800

    feat: improve `error` page

--- src/views/error.vue
+++ src/views/error.vue
@@ -1,10 +1,20 @@
 <template>
-  <div>Error</div>
+  <div class="app-container">
+    <el-card>
+      <div slot="header">提醒</div>
+      <el-alert type="error">获取模块失败，<el-button type="text" @click="doReloadPage">点我</el-button>刷新页面</el-alert>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Error'
+  name: 'Error',
+  methods: {
+    doReloadPage () {
+      window.location.reload()
+    }
+  }
 }
 </script>
 

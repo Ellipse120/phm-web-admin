e71d349fb45e4b58272f2e21d430fa7732a7c59f
路赛 <lusai@outlook.com>
2021-03-24 14:12:16 +0800

    feat: import `tinymce`

--- src/views/real-time-state/index.vue
+++ src/views/real-time-state/index.vue
@@ -8,14 +8,30 @@
       </template>
     </el-tabs>
     <a-button type="danger">ant button</a-button>
+    <tinymce-editor
+      v-model="content"
+      :tinymce-script-src="localSrc"
+      api-key="test"
+      :init="{
+        height: 500,
+        menubar: false,
+        language: 'zh_CN',
+        plugins: plugins,
+        toolbar: toolbar,
+        statusbar: false
+      }"
+    />
+    {{ content }}
   </div>
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue'
+
 export default {
   name: 'RealTimeState',
-
   components: {
+    tinymceEditor: Editor,
     analysis: () => import('./analysis'),
     monitor: () => import('./monitor'),
     baseInfo: () => import('./base-info'),
@@ -24,6 +40,12 @@ export default {
   },
   data () {
     return {
+      content: null,
+      // https://www.tiny.cloud/get-tiny/language-packages/ 下载语言包放到 /langs 目录下
+      // 下载 https://www.tiny.cloud/get-tiny/self-hosted/ js包部署私服
+      localSrc: 'http://172.1.128.184:8080/js/tinymce/tinymce.min.js',
+      plugins: ['advlist anchor autolink autosave code codesample directionality emoticons fullscreen hr image imagetools insertdatetime link lists media nonbreaking noneditable pagebreak paste preview print save searchreplace spellchecker tabfocus table template textpattern visualblocks visualchars wordcount'],
+      toolbar: ['searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample', 'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen'],
       tabs: [
         {
           label: '当前故障',
